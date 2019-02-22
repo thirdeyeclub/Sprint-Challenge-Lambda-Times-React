326c02e20bb249e464278d7d30a2619ae478e39a
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-02-22 13:34:09 -0500

    Fixed filter and Did Card MVP

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -39,12 +39,14 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-   if (this.state.selected === 'all') {
-    return this.state.cards;
+
+  if (this.state.selected==="all"){
+    return this.state.cards
+  } else {
+    let myCards=this.state.cards.filter(card =>
+      card.tab===this.state.selected)
+      return myCards;
   }
-  return (this.state.cards.filter(card => {
-    return card.tab === this.state.selected
-                                          }));
 };
 
   render() {
